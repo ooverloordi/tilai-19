1ec44f9f7b62684d74df6a93017ee25a89fc6ab8
Antti Pollari <pollari@iki.fi>
2019-10-16 13:54:41 +0300

    /sigh

--- build.sh
+++ build.sh
@@ -14,15 +14,15 @@ cd ~/tilai-19/
 # build website
 bundle exec middleman build
 
-# move the build website on top of the website branch of repo
+# move the built website on top of the website branch of repo
 cp -r ~/tilai-19/build/* ~/gh-pages/tilai-19/
 rm -R ./build
 
-# Website branch commit & push
+# Website branch commit & push (with a random witty comment)
 git -C ~/gh-pages/tilai-19/ commit -a -m "$(curl -s whatthecommit.com/index.txt)"
 git -C ~/gh-pages/tilai-19/ push
 
-# Main branch commit & push
+# Main branch commit & push (with even more random witty comment)
 git commit -a -m "$(curl -s whatthecommit.com/index.txt)"
 git push
 
